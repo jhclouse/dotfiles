733f8b547b44f84d02b34919d27b8119a3e87e27
Jason Clouse <jhclouse@gmail.com>
2021-07-06 13:06:00 -0400

    Added gitignore

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ ln -sf $PWD/zshrc $HOME/.zshrc
 ln -sf $PWD/zshenv $HOME/.zshenv
 ln -sf $PWD/gvimrc $HOME/.gvimrc
 ln -sf $PWD/vimrc $HOME/.vimrc
+ln -sf $PWD/gitignore $HOME/.gitignore
 ln -sf $PWD/gitconfig $HOME/.gitconfig
 ln -sf $PWD/vim $HOME/.vim
