a0d4e38dc07e19cc6f639df8a62f2d23841771e1
Jason Clouse <jhclouse@gmail.com>
2022-12-01 15:30:57 -0500

    Resorted files in install to match `ls` order and added .ackrc

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 # Symlink files in home directory
 
-ln -sf $PWD/zshrc $HOME/.zshrc
-ln -sf $PWD/zshenv $HOME/.zshenv
-ln -sf $PWD/gvimrc $HOME/.gvimrc
-ln -sf $PWD/vimrc $HOME/.vimrc
-ln -sf $PWD/gitignore $HOME/.gitignore
+ln -sf $PWD/ackrc $HOME/.ackrc
 ln -sf $PWD/gitconfig $HOME/.gitconfig
+ln -sf $PWD/gitignore $HOME/.gitignore
+ln -sf $PWD/gvimrc $HOME/.gvimrc
 ln -sf $PWD/vim $HOME/.vim
+ln -sf $PWD/vimrc $HOME/.vimrc
+ln -sf $PWD/zshenv $HOME/.zshenv
+ln -sf $PWD/zshrc $HOME/.zshrc
